8c1289ac7691820c18aa7fd1cc8c475aaafc9afb
WhitePiano <startoflateness@gmail.com>
2025-01-10 20:37:14 +0900

    chore(*): spring initializr

---
+++ src/main/java/sleppynavigators/studyupbackend/StudyUpBackendApplication.java
@@ -0,0 +1,13 @@
+package sleppynavigators.studyupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyUpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyUpBackendApplication.class, args);
+    }
+
+}
