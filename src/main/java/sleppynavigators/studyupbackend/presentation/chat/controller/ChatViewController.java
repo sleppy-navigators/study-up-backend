d1eecadc504a026bb3cd24052481bd7a6efbcd02
pjy1368 <basejin0810@naver.com>
2025-01-29 22:53:37 +0900

    feat: 테스트용 뷰 작성

---
+++ src/main/java/sleppynavigators/studyupbackend/presentation/chat/controller/ChatViewController.java
@@ -0,0 +1,13 @@
+package sleppynavigators.studyupbackend.presentation.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatViewController {
+
+    @GetMapping("/chat")
+    public String chatView() {
+        return "chat";
+    }
+} 
