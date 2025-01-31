b53537e72b041bd13374c46a4e7595c8689714b7
WhitePiano <startoflateness@gmail.com>
2025-01-31 16:47:32 +0900

    refactor(auth): separate userSession's PK

--- src/main/java/sleppynavigators/studyupbackend/domain/authentication/session/UserSession.java
+++ src/main/java/sleppynavigators/studyupbackend/domain/authentication/session/UserSession.java
@@ -3,9 +3,10 @@ package sleppynavigators.studyupbackend.domain.authentication.session;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -18,9 +19,9 @@ import sleppynavigators.studyupbackend.domain.user.User;
 public class UserSession {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @MapsId
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
