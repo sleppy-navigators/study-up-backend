465956dd7591fac47b44cc5f257f22bf6e450806
WhitePiano <startoflateness@gmail.com>
2025-02-14 02:14:23 +0900

    test(group): inject mock user

--- src/test/java/sleppynavigators/studyupbackend/presentation/group/GroupControllerTest.java
+++ src/test/java/sleppynavigators/studyupbackend/presentation/group/GroupControllerTest.java
@@ -8,6 +8,7 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
+import java.util.List;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,18 +18,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import sleppynavigators.studyupbackend.domain.authentication.token.AccessToken;
+import sleppynavigators.studyupbackend.domain.authentication.token.AccessTokenProperties;
 import sleppynavigators.studyupbackend.domain.user.User;
 import sleppynavigators.studyupbackend.domain.user.vo.UserProfile;
 import sleppynavigators.studyupbackend.infrastructure.group.GroupRepository;
 import sleppynavigators.studyupbackend.infrastructure.user.UserRepository;
 import sleppynavigators.studyupbackend.presentation.common.SuccessResponse;
-import sleppynavigators.studyupbackend.presentation.common.WithMockedUserInfo;
 import sleppynavigators.studyupbackend.presentation.group.dto.GroupCreationRequest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@WithMockedUserInfo
-@DisplayName("GroupController 테스트")
+@DisplayName("GroupController API 테스트")
 public class GroupControllerTest {
 
     @Autowired
@@ -37,23 +38,32 @@ public class GroupControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AccessTokenProperties accessTokenProperties;
+
     @LocalServerPort
     private int port;
 
     @BeforeEach
     void setUp() {
-        userRepository.save(new User(new UserProfile("guest", "example@guest.com")));
+        UserProfile userProfile = new UserProfile("guest", "example@guest.com");
+        User savedUser = userRepository.saveAndFlush(new User(userProfile));
+
+        AccessToken accessToken =
+                new AccessToken(savedUser.getId(), userProfile, List.of("profile"), accessTokenProperties);
+        String bearerToken = "Bearer " + accessToken.serialize(accessTokenProperties);
 
         RestAssured.port = port;
         RestAssured.requestSpecification = new RequestSpecBuilder()
                 .setContentType(ContentType.JSON)
+                .addHeader("Authorization", bearerToken)
                 .build();
     }
 
     @AfterEach
     void tearDown() {
-        userRepository.deleteAll();
         groupRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
