1d6e4165c9a64de8f36f1b87f367c4f800b4f34d
WhitePiano <startoflateness@gmail.com>
2025-02-14 02:13:36 +0900

    fix(group): remove unmatch validation

--- src/main/java/sleppynavigators/studyupbackend/presentation/group/dto/GroupCreationRequest.java
+++ src/main/java/sleppynavigators/studyupbackend/presentation/group/dto/GroupCreationRequest.java
@@ -1,13 +1,12 @@
 package sleppynavigators.studyupbackend.presentation.group.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import sleppynavigators.studyupbackend.domain.group.Group;
 import sleppynavigators.studyupbackend.domain.user.User;
 
 public record GroupCreationRequest(@NotBlank String name,
                                    @NotBlank String description,
-                                   @Email String thumbnailUrl) {
+                                   String thumbnailUrl) {
 
     public Group toEntity(User creator) {
         return Group.builder()
