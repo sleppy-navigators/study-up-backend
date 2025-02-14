d221b3a86fa1ee3f69c5341d649743391b43d4aa
WhitePiano <startoflateness@gmail.com>
2025-02-14 19:16:27 +0900

    test(auth): remove mocked from API test

--- src/test/java/sleppynavigators/studyupbackend/presentation/authentication/AuthControllerTest.java
+++ src/test/java/sleppynavigators/studyupbackend/presentation/authentication/AuthControllerTest.java
@@ -3,10 +3,7 @@ package sleppynavigators.studyupbackend.presentation.authentication;
 import static io.restassured.RestAssured.with;
 import static io.restassured.http.Method.POST;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
@@ -18,16 +15,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import sleppynavigators.studyupbackend.domain.authentication.UserCredential;
 import sleppynavigators.studyupbackend.domain.authentication.session.UserSession;
 import sleppynavigators.studyupbackend.domain.authentication.token.AccessToken;
 import sleppynavigators.studyupbackend.domain.authentication.token.AccessTokenProperties;
@@ -35,13 +26,9 @@ import sleppynavigators.studyupbackend.domain.authentication.token.RefreshToken;
 import sleppynavigators.studyupbackend.domain.user.User;
 import sleppynavigators.studyupbackend.exception.ErrorCode;
 import sleppynavigators.studyupbackend.exception.ErrorResponse;
-import sleppynavigators.studyupbackend.infrastructure.authentication.UserCredentialRepository;
-import sleppynavigators.studyupbackend.infrastructure.authentication.oidc.GoogleOidcClient;
 import sleppynavigators.studyupbackend.infrastructure.authentication.session.UserSessionRepository;
 import sleppynavigators.studyupbackend.infrastructure.user.UserRepository;
 import sleppynavigators.studyupbackend.presentation.authentication.dto.request.RefreshRequest;
-import sleppynavigators.studyupbackend.presentation.authentication.dto.request.SignInRequest;
-import sleppynavigators.studyupbackend.exception.network.InvalidCredentialException;
 import sleppynavigators.studyupbackend.presentation.common.DatabaseCleaner;
 import sleppynavigators.studyupbackend.presentation.common.SuccessResponse;
 
@@ -51,9 +38,6 @@ import sleppynavigators.studyupbackend.presentation.common.SuccessResponse;
 class AuthControllerTest {
 
     @Autowired
-    private UserCredentialRepository userCredentialRepository;
-
-    @Autowired
     private AccessTokenProperties accessTokenProperties;
 
     @Autowired
@@ -61,9 +45,6 @@ class AuthControllerTest {
 
     @Autowired
     private UserRepository userRepository;
-
-    @MockitoBean
-    private GoogleOidcClient googleOidcClient;
 
     @Autowired
     private DatabaseCleaner databaseCleaner;
@@ -84,94 +65,6 @@ class AuthControllerTest {
         databaseCleaner.execute();
     }
 
-    @TestConfiguration
-    static class TestConfig {
-
-        @Primary
-        @Bean
-        public GoogleOidcClient googleOidcClient() {
-            return Mockito.mock(GoogleOidcClient.class);
-        }
-    }
-
-    @Test
-    @DisplayName("기존 회원의 구글 로그인을 성공적으로 수행한다")
-    void memberGoogleSignIn_Success() {
-        // given
-        String idToken = "test-id-token";
-        SignInRequest request = new SignInRequest(idToken);
-        Claims idTokenClaims = Jwts.claims()
-                .subject("test-subject")
-                .add("name", "test-user")
-                .add("email", "test-email")
-                .build();
-        given(googleOidcClient.deserialize(idToken)).willReturn(idTokenClaims);
-
-        User user = new User("test-user", "test-email");
-        UserCredential userCredential = new UserCredential("test-subject", "google", user);
-        userCredentialRepository.save(userCredential);
-
-        // when
-        ExtractableResponse<?> response = with()
-                .body(request).queryParam("provider", "GOOGLE")
-                .when().request(POST, "/auth/sign-in")
-                .then()
-                .log().all().extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
-        assertThat(response.body().as(SuccessResponse.class).getData()).isNotNull();
-    }
-
-    @Test
-    @DisplayName("신규 회원의 구글 로그인을 성공적으로 수행한다")
-    void newMemberGoogleSignIn_Success() {
-        // given
-        String idToken = "test-id-token";
-        SignInRequest request = new SignInRequest(idToken);
-        Claims idTokenClaims = Jwts.claims()
-                .subject("test-subject")
-                .add("name", "test-user")
-                .add("email", "test-email")
-                .build();
-        given(googleOidcClient.deserialize(idToken)).willReturn(idTokenClaims);
-
-        assert userCredentialRepository.findBySubject("test-subject").isEmpty();
-
-        // when
-        ExtractableResponse<?> response = with()
-                .body(request).queryParam("provider", "GOOGLE")
-                .when().request(POST, "/auth/sign-in")
-                .then()
-                .log().all().extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
-        assertThat(response.body().as(SuccessResponse.class).getData()).isNotNull();
-        assertThat(userCredentialRepository.findBySubject("test-subject")).isNotEmpty();
-    }
-
-    @Test
-    @DisplayName("유효하지 않은 id token으로 구글 로그인을 시도하면 예외가 발생한다")
-    void whenInvalidIdToken_ThrowsInvalidCredentialException() {
-        // given
-        String idToken = "invalid-id-token";
-        SignInRequest request = new SignInRequest(idToken);
-        given(googleOidcClient.deserialize(idToken)).willThrow(new InvalidCredentialException());
-
-        // when
-        ExtractableResponse<?> response = with()
-                .body(request).queryParam("provider", "GOOGLE")
-                .when().request(POST, "/auth/sign-in")
-                .then()
-                .log().all().extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
-        assertThat(response.body().as(ErrorResponse.class).getCode())
-                .isEqualTo(ErrorCode.INVALID_CREDENTIALS.getCode());
-    }
-
     @Test
     @DisplayName("토큰 갱신 요청이 성공적으로 수행된다")
     void refresh_Success() {
@@ -179,12 +72,12 @@ class AuthControllerTest {
         User user = new User("test-user", "test-email");
         userRepository.save(user);
 
-        AccessToken accessToken = new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"),
-                accessTokenProperties);
+        AccessToken accessToken =
+                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
         RefreshToken refreshToken = new RefreshToken();
         LocalDateTime notExpiredTime = LocalDateTime.now().plusMinutes(1);
-        RefreshRequest request = new RefreshRequest(accessToken.serialize(accessTokenProperties),
-                refreshToken.serialize());
+        RefreshRequest request =
+                new RefreshRequest(accessToken.serialize(accessTokenProperties), refreshToken.serialize());
 
         UserSession userSession = UserSession.builder()
                 .user(user)
@@ -212,12 +105,12 @@ class AuthControllerTest {
         User user = new User("test-user", "test-email");
         userRepository.save(user);
 
-        AccessToken accessToken = new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"),
-                accessTokenProperties);
+        AccessToken accessToken =
+                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
         RefreshToken refreshToken = new RefreshToken();
         LocalDateTime expiredTime = LocalDateTime.now().minusMinutes(1);
-        RefreshRequest request = new RefreshRequest(accessToken.serialize(accessTokenProperties),
-                refreshToken.serialize());
+        RefreshRequest request =
+                new RefreshRequest(accessToken.serialize(accessTokenProperties), refreshToken.serialize());
 
         UserSession userSession = UserSession.builder()
                 .user(user)
@@ -246,8 +139,8 @@ class AuthControllerTest {
         User user = new User("test-user", "test-email");
         userRepository.save(user);
 
-        AccessToken accessToken = new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"),
-                accessTokenProperties);
+        AccessToken accessToken =
+                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
         RefreshToken refreshToken = new RefreshToken();
         LocalDateTime notExpiredTime = LocalDateTime.now().plusMinutes(1);
         RefreshRequest request = new RefreshRequest("invalid-access-token", "invalid-refresh-token");
