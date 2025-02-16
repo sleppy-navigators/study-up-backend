5dada6499e901368cd8b81075670097cb4f73ba4
pjy1368 <basejin0810@naver.com>
2025-02-16 13:38:02 +0900

    feat: ChatService 구현

--- src/main/java/sleppynavigators/studyupbackend/presentation/chat/dto/ChatMessageResponse.java
+++ src/main/java/sleppynavigators/studyupbackend/presentation/chat/dto/ChatMessageResponse.java
@@ -2,12 +2,23 @@ package sleppynavigators.studyupbackend.presentation.chat.dto;
 
 import java.time.LocalDateTime;
 import lombok.Builder;
+import sleppynavigators.studyupbackend.domain.chat.ChatMessage;
 
 @Builder
 public record ChatMessageResponse(
+        String messageId,
         Long groupId,
         Long senderId,
         String content,
         LocalDateTime timestamp
 ) {
+    public static ChatMessageResponse from(ChatMessage chatMessage) {
+        return ChatMessageResponse.builder()
+                .messageId(chatMessage.getId().toString())
+                .groupId(chatMessage.getGroupId())
+                .senderId(chatMessage.getSenderId())
+                .content(chatMessage.getContent())
+                .timestamp(chatMessage.getCreatedAt())
+                .build();
+    }
 }
