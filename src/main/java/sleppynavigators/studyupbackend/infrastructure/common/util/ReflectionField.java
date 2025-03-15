258a75ad7b0ae892f30ec7d031c19167e08ab17b
WhitePiano <startoflateness@gmail.com>
2025-03-15 22:55:28 +0900

    refactor(*): utilize cache to reflect field only once per entity

--- src/main/java/sleppynavigators/studyupbackend/infrastructure/common/util/ReflectionField.java
+++ src/main/java/sleppynavigators/studyupbackend/infrastructure/common/util/ReflectionField.java
@@ -1,26 +1,26 @@
 package sleppynavigators.studyupbackend.infrastructure.common.util;
 
 import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 public class ReflectionField<T> {
 
-    private final Field field;
+    private static final Map<Class<?>, Field> cache = new ConcurrentHashMap<>();
+
+    private final Class<T> fieldClazz;
     private final Object instance;
 
     public ReflectionField(Object instance, Class<T> fieldClazz) {
         this.instance = instance;
-        this.field = Stream.of(instance.getClass().getDeclaredFields())
-                .filter(field -> field.getType().equals(fieldClazz))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
-        field.setAccessible(true);
+        this.fieldClazz = fieldClazz;
     }
 
     @SuppressWarnings("unchecked")
     public T get() {
         try {
-            return (T) field.get(instance);
+            return (T) getField().get(instance);
         } catch (IllegalAccessException ex) {
             throw new RuntimeException(ex);
         }
@@ -28,10 +28,20 @@ public class ReflectionField<T> {
 
     public void set(T value) {
         try {
-            field.setAccessible(true);
-            field.set(instance, value);
+            getField().set(instance, value);
         } catch (IllegalAccessException ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    private Field getField() {
+        return cache.computeIfAbsent(instance.getClass(), clazz -> {
+            Field target = Stream.of(instance.getClass().getDeclaredFields())
+                    .filter(field -> field.getType().equals(fieldClazz))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+            target.setAccessible(true);
+            return target;
+        });
     }
 }
