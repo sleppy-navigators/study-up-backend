3ac300c2a60c354bb33f03924237bb979499d296
WhitePiano <startoflateness@gmail.com>
2025-03-29 17:01:47 +0900

    refactor(auth): extract test fixture factory

--- src/test/java/sleppynavigators/studyupbackend/common/RestAssuredBaseTest.java
+++ src/test/java/sleppynavigators/studyupbackend/common/RestAssuredBaseTest.java
@@ -3,10 +3,15 @@ package sleppynavigators.studyupbackend.common;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 public class RestAssuredBaseTest extends ApplicationBaseTest {
+
+    protected Validator validator;
 
     @LocalServerPort
     private int port;
@@ -15,6 +20,11 @@ public class RestAssuredBaseTest extends ApplicationBaseTest {
     @BeforeEach
     void setUp() {
         super.setUp();
+
+        // Initialize the validator using the default factory
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
 
         // Set up RestAssured with the local server port and JSON content type
         RestAssured.reset();
--- src/test/java/sleppynavigators/studyupbackend/presentation/authentication/AuthControllerTest.java
+++ src/test/java/sleppynavigators/studyupbackend/presentation/authentication/AuthControllerTest.java
@@ -17,12 +17,13 @@ import sleppynavigators.studyupbackend.domain.authentication.token.AccessToken;
 import sleppynavigators.studyupbackend.domain.authentication.token.AccessTokenProperties;
 import sleppynavigators.studyupbackend.domain.authentication.token.RefreshToken;
 import sleppynavigators.studyupbackend.domain.user.User;
+import sleppynavigators.studyupbackend.domain.user.vo.UserProfile;
 import sleppynavigators.studyupbackend.exception.ErrorCode;
 import sleppynavigators.studyupbackend.infrastructure.authentication.session.UserSessionRepository;
 import sleppynavigators.studyupbackend.infrastructure.user.UserRepository;
 import sleppynavigators.studyupbackend.presentation.authentication.dto.request.RefreshRequest;
-import sleppynavigators.studyupbackend.presentation.common.DatabaseCleaner;
 import sleppynavigators.studyupbackend.common.RestAssuredBaseTest;
+import sleppynavigators.studyupbackend.presentation.authentication.dto.response.TokenResponse;
 
 @DisplayName("AuthController API 테스트")
 class AuthControllerTest extends RestAssuredBaseTest {
@@ -40,23 +41,13 @@ class AuthControllerTest extends RestAssuredBaseTest {
     @DisplayName("토큰 갱신 요청이 성공적으로 수행된다")
     void refresh_Success() {
         // given
-        User user = new User("test-user", "test-email");
-        userRepository.save(user);
-
-        AccessToken accessToken =
-                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
-        RefreshToken refreshToken = new RefreshToken();
+        User userToRefresh = TestFixtureMother.registerUser(userRepository);
         LocalDateTime notExpiredTime = LocalDateTime.now().plusMinutes(1);
-        RefreshRequest request =
-                new RefreshRequest(accessToken.serialize(accessTokenProperties), refreshToken.serialize());
+        UserSession sessionToRefresh = TestFixtureMother
+                .registerUserSession(userSessionRepository, userToRefresh, notExpiredTime, accessTokenProperties);
 
-        UserSession userSession = UserSession.builder()
-                .user(user)
-                .refreshToken(refreshToken.serialize())
-                .accessToken(accessToken.serialize(accessTokenProperties))
-                .expiration(notExpiredTime)
-                .build();
-        userSessionRepository.save(userSession);
+        RefreshRequest request =
+                new RefreshRequest(sessionToRefresh.getAccessToken(), sessionToRefresh.getRefreshToken());
 
         // when
         ExtractableResponse<?> response = with().body(request)
@@ -66,31 +57,21 @@ class AuthControllerTest extends RestAssuredBaseTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
-        assertThat(response.jsonPath().getString("data.accessToken")).isNotBlank();
-        assertThat(response.jsonPath().getString("data.refreshToken")).isNotBlank();
+        assertThat(response.jsonPath().getObject("data", TokenResponse.class))
+                .satisfies(data -> assertThat(this.validator.validate(data)).isEmpty());
     }
 
     @Test
     @DisplayName("만료된 세션에 대해 토큰 갱신 요청을 수행하면 예외가 발생한다")
     void whenExpiredSession_ThrowsInvalidCredentialException() {
         // given
-        User user = new User("test-user", "test-email");
-        userRepository.save(user);
-
-        AccessToken accessToken =
-                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
-        RefreshToken refreshToken = new RefreshToken();
+        User userToRefresh = TestFixtureMother.registerUser(userRepository);
         LocalDateTime expiredTime = LocalDateTime.now().minusMinutes(1);
-        RefreshRequest request =
-                new RefreshRequest(accessToken.serialize(accessTokenProperties), refreshToken.serialize());
+        UserSession sessionToRefresh = TestFixtureMother
+                .registerUserSession(userSessionRepository, userToRefresh, expiredTime, accessTokenProperties);
 
-        UserSession userSession = UserSession.builder()
-                .user(user)
-                .refreshToken(refreshToken.serialize())
-                .accessToken(accessToken.serialize(accessTokenProperties))
-                .expiration(expiredTime)
-                .build();
-        userSessionRepository.save(userSession);
+        RefreshRequest request =
+                new RefreshRequest(sessionToRefresh.getAccessToken(), sessionToRefresh.getRefreshToken());
 
         // when
         ExtractableResponse<?> response = with().body(request)
@@ -108,14 +89,10 @@ class AuthControllerTest extends RestAssuredBaseTest {
     @DisplayName("존재하지 않는 세션에 대해 토큰 갱신 요청을 수행하면 예외가 발생한다")
     void whenNonExistentSession_ThrowsInvalidCredentialException() {
         // given
-        User user = new User("test-user", "test-email");
-        userRepository.save(user);
-
-        AccessToken accessToken =
-                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
-        RefreshToken refreshToken = new RefreshToken();
-        RefreshRequest request =
-                new RefreshRequest(accessToken.serialize(accessTokenProperties), refreshToken.serialize());
+        User userToRefresh = TestFixtureMother.registerUser(userRepository);
+        String accessToken = TestFixtureMother.createAccessToken(userToRefresh, accessTokenProperties);
+        String refreshToken = TestFixtureMother.createRefreshToken();
+        RefreshRequest request = new RefreshRequest(accessToken, refreshToken);
 
         // when
         ExtractableResponse<?> response = with().body(request)
@@ -134,27 +111,14 @@ class AuthControllerTest extends RestAssuredBaseTest {
     @DisplayName("토큰 정보가 일치하지 않을 경우 예외가 발생한다")
     void whenUnMatchedToken_ThrowsInvalidCredentialException() {
         // given
-        User user = new User("test-user", "test-email");
-        userRepository.save(user);
-
-        AccessToken accessToken =
-                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
-        RefreshToken refreshToken = new RefreshToken();
+        User userToRefresh = TestFixtureMother.registerUser(userRepository);
         LocalDateTime notExpiredTime = LocalDateTime.now().plusMinutes(1);
+        UserSession sessionToRefresh = TestFixtureMother
+                .registerUserSession(userSessionRepository, userToRefresh, notExpiredTime, accessTokenProperties);
 
-        AccessToken invalidAccessToken =
-                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
-        RefreshToken invalidRefreshToken = new RefreshToken();
-        RefreshRequest request = new RefreshRequest(
-                invalidAccessToken.serialize(accessTokenProperties), invalidRefreshToken.serialize());
-
-        UserSession userSession = UserSession.builder()
-                .user(user)
-                .refreshToken(refreshToken.serialize())
-                .accessToken(accessToken.serialize(accessTokenProperties))
-                .expiration(notExpiredTime)
-                .build();
-        userSessionRepository.save(userSession);
+        String accessToken = TestFixtureMother.createAccessToken(userToRefresh, accessTokenProperties);
+        String refreshToken = TestFixtureMother.createRefreshToken();
+        RefreshRequest request = new RefreshRequest(accessToken, refreshToken);
 
         // when
         ExtractableResponse<?> response = with().body(request)
@@ -173,23 +137,13 @@ class AuthControllerTest extends RestAssuredBaseTest {
     @DisplayName("JWT가 아닌 Access 토큰으로 토큰 갱신 요청을 수행하면 예외가 발생한다")
     void whenNotJwtAccessToken_ThrowsInvalidCredentialException() {
         // given
-        User user = new User("test-user", "test-email");
-        userRepository.save(user);
-
-        AccessToken accessToken =
-                new AccessToken(user.getId(), user.getUserProfile(), List.of("profile"), accessTokenProperties);
-        RefreshToken refreshToken = new RefreshToken();
+        User userToRefresh = TestFixtureMother.registerUser(userRepository);
         LocalDateTime notExpiredTime = LocalDateTime.now().plusMinutes(1);
-        RefreshRequest request =
-                new RefreshRequest("not-jwt-access-token", refreshToken.serialize());
+        UserSession sessionToRefresh = TestFixtureMother
+                .registerUserSession(userSessionRepository, userToRefresh, notExpiredTime, accessTokenProperties);
 
-        UserSession userSession = UserSession.builder()
-                .user(user)
-                .refreshToken(refreshToken.serialize())
-                .accessToken(accessToken.serialize(accessTokenProperties))
-                .expiration(notExpiredTime)
-                .build();
-        userSessionRepository.save(userSession);
+        RefreshRequest request =
+                new RefreshRequest("not-jwt-access-token", sessionToRefresh.getRefreshToken());
 
         // when
         ExtractableResponse<?> response = with().body(request)
@@ -202,5 +156,46 @@ class AuthControllerTest extends RestAssuredBaseTest {
         assertThat(response.jsonPath().getString("code")).isEqualTo(ErrorCode.INVALID_CREDENTIALS.getCode());
         assertThat(response.jsonPath().getString("message")).isEqualTo(
                 ErrorCode.INVALID_CREDENTIALS.getDefaultMessage());
+    }
+
+    @Transactional
+    static class TestFixtureMother {
+
+        static User registerUser(UserRepository userRepository) {
+            User user = new User("test-user", "test-email");
+            return userRepository.save(user);
+        }
+
+        static UserSession registerUserSession(UserSessionRepository userSessionRepository,
+                                               User user, LocalDateTime expiration,
+                                               AccessTokenProperties accessTokenProperties) {
+            Long userId = user.getId();
+            UserProfile userProfile = user.getUserProfile();
+            List<String> authorities = List.of("profile");
+
+            AccessToken accessToken = new AccessToken(userId, userProfile, authorities, accessTokenProperties);
+            RefreshToken refreshToken = new RefreshToken();
+
+            UserSession userSession = UserSession.builder()
+                    .user(user)
+                    .refreshToken(refreshToken.serialize())
+                    .accessToken(accessToken.serialize(accessTokenProperties))
+                    .expiration(expiration)
+                    .build();
+            return userSessionRepository.save(userSession);
+        }
+
+        static String createAccessToken(User user, AccessTokenProperties accessTokenProperties) {
+            Long userId = user.getId();
+            UserProfile userProfile = user.getUserProfile();
+            List<String> authorities = List.of("profile");
+
+            return new AccessToken(userId, userProfile, authorities, accessTokenProperties)
+                    .serialize(accessTokenProperties);
+        }
+
+        static String createRefreshToken() {
+            return new RefreshToken().serialize();
+        }
     }
 }
