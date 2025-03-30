c5dbd735d2e259d32974aa91fdf027bbe37a4515
pjy1368 <basejin0810@naver.com>
2025-03-30 14:38:45 +0900

    refactor: sealed 제거

--- src/main/java/sleppynavigators/studyupbackend/domain/event/SystemEvent.java
+++ src/main/java/sleppynavigators/studyupbackend/domain/event/SystemEvent.java
@@ -2,11 +2,7 @@ package sleppynavigators.studyupbackend.domain.event;
 
 import sleppynavigators.studyupbackend.domain.chat.SystemMessageTemplate;
 
-public sealed interface SystemEvent permits
-        UserJoinEvent,
-        UserLeaveEvent,
-        ChallengeCreateEvent,
-        ChallengeCompleteEvent {
+public interface SystemEvent {
 
     SystemEventType getType();
     Long getGroupId();
