9aca96c1990a8e65818029b717b665bc05953864
WhitePiano <startoflateness@gmail.com>
2025-04-11 22:31:12 +0900

    feat(challenge): add filtering option to query challenge tasks

--- src/main/java/sleppynavigators/studyupbackend/presentation/common/config/WebConfig.java
+++ src/main/java/sleppynavigators/studyupbackend/presentation/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package sleppynavigators.studyupbackend.presentation.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import sleppynavigators.studyupbackend.presentation.challenge.dto.request.TaskSearchArgumentResolver;
 import sleppynavigators.studyupbackend.presentation.chat.dto.request.ChatMessageSearchArgumentResolver;
 
 import java.util.List;
@@ -12,6 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new ChatMessageSearchArgumentResolver());
+        resolvers.addAll(
+                List.of(
+                        new ChatMessageSearchArgumentResolver(),
+                        new TaskSearchArgumentResolver()));
     }
 }
