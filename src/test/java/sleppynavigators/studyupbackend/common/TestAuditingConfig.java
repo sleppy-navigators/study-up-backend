642d3475bc9cba7b17c42d1a12092bfab9ae2d4d
WhitePiano <startoflateness@gmail.com>
2025-04-30 23:10:55 +0900

    test(*): apply test auditor aware

--- src/test/java/sleppynavigators/studyupbackend/common/TestAuditingConfig.java
+++ src/test/java/sleppynavigators/studyupbackend/common/TestAuditingConfig.java
@@ -2,6 +2,7 @@ package sleppynavigators.studyupbackend.common;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.AuditorAware;
 import java.util.Optional;
@@ -11,6 +12,7 @@ import java.util.Optional;
 public class TestAuditingConfig {
 
     @Bean
+    @Primary
     public AuditorAware<Long> testAuditorProvider() {
         return () -> Optional.of(1L);
     }
