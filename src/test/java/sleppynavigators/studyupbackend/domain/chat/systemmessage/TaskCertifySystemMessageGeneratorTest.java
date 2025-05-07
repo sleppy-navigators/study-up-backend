b3aa6827125cb6128191be9b0db00be8f9765eb4
WhitePiano <startoflateness@gmail.com>
2025-05-07 18:41:32 +0900

    test(challenge): update test codes

---
+++ src/test/java/sleppynavigators/studyupbackend/domain/chat/systemmessage/TaskCertifySystemMessageGeneratorTest.java
@@ -0,0 +1,37 @@
+package sleppynavigators.studyupbackend.domain.chat.systemmessage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import sleppynavigators.studyupbackend.domain.event.EventType;
+import sleppynavigators.studyupbackend.domain.event.TaskCertifiedEvent;
+
+public class TaskCertifySystemMessageGeneratorTest {
+
+    private final TaskCertifySystemMessageGenerator generator = new TaskCertifySystemMessageGenerator();
+
+    @Test
+    void getEventType_ShouldReturnTaskCertify() {
+        // when
+        EventType eventType = generator.supportedEventType();
+
+        // then
+        assertThat(eventType).isEqualTo(EventType.TASK_CERTIFY);
+    }
+
+    @Test
+    void generate_ShouldReturnFormattedMessage() {
+        // given
+        String userName = "홍길동";
+        String challengeName = "알고리즘 끝장내기";
+        String taskName = "알고리즘 문제 풀기";
+        Long groupId = 1L;
+        TaskCertifiedEvent event = new TaskCertifiedEvent(userName, challengeName, taskName, groupId);
+
+        // when
+        String message = generator.generate(event);
+
+        // then
+        assertThat(message).isEqualTo("홍길동님이 '알고리즘 문제 풀기' 테스크를 완료했습니다. (알고리즘 끝장내기)");
+    }
+}
