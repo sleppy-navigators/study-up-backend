ee03259b577d27bb78c01be992469f8fe2b9ba4b
WhitePiano <startoflateness@gmail.com>
2025-05-14 01:29:06 +0900

    Docs : expose query strings more readable (#58)

--- src/main/java/sleppynavigators/studyupbackend/presentation/challenge/dto/request/ChallengeSearch.java
+++ src/main/java/sleppynavigators/studyupbackend/presentation/challenge/dto/request/ChallengeSearch.java
@@ -2,8 +2,10 @@ package sleppynavigators.studyupbackend.presentation.challenge.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.hibernate.validator.constraints.Range;
+import org.springdoc.core.annotations.ParameterObject;
 import sleppynavigators.studyupbackend.application.challenge.ChallengeSortType;
 
+@ParameterObject
 @Schema(description = "챌린지 검색 조건")
 public record ChallengeSearch(
         @Schema(description = "페이지 번호", example = "0")
