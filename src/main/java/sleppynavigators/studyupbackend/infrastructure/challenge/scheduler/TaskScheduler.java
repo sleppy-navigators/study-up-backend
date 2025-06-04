7547633f0fcbe9d3ea91ffc9ce7c698d092e1570
WhitePiano <startoflateness@gmail.com>
2025-06-04 15:44:02 +0900

    feat(challenge): notify when task failed

---
+++ src/main/java/sleppynavigators/studyupbackend/infrastructure/challenge/scheduler/TaskScheduler.java
@@ -0,0 +1,52 @@
+package sleppynavigators.studyupbackend.infrastructure.challenge.scheduler;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import sleppynavigators.studyupbackend.application.event.NotificationEventPublisher;
+import sleppynavigators.studyupbackend.domain.challenge.Task;
+import sleppynavigators.studyupbackend.domain.event.TaskFailEvent;
+import sleppynavigators.studyupbackend.infrastructure.challenge.TaskQueryOptions;
+import sleppynavigators.studyupbackend.infrastructure.challenge.TaskRepository;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class TaskScheduler {
+
+    private final NotificationEventPublisher notificationEventPublisher;
+    private final TaskRepository taskRepository;
+
+    @Value("${scheduler.challenge.check-expiration.interval-minutes}")
+    private long challengeCheckIntervalMinutes;
+
+    @Scheduled(cron = "${scheduler.challenge.check-expiration.cron}", zone = "Asia/Seoul")
+    @Transactional
+    public void checkFailedTasks() {
+        log.info("ChallengeScheduler - checkExpiredTasks() started");
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime baseTime = now.minusMinutes(challengeCheckIntervalMinutes);
+        BooleanExpression predicate = TaskQueryOptions.getCompletedBetweenPredicate(baseTime, now);
+        List<Task> expiredTasks = taskRepository.findAll(predicate);
+
+        for (Task task : expiredTasks) {
+            log.info("ChallengeScheduler - Processing expired task: {}", task.getId());
+            TaskFailEvent event = new TaskFailEvent(
+                    task.getChallenge().getOwner().getUserProfile().getUsername(),
+                    task.getChallenge().getDetail().getTitle(),
+                    task.getChallenge().getGroup().getId(),
+                    task.getChallenge().getId(),
+                    task.getChallenge().getOwner().getId()
+            );
+            notificationEventPublisher.publish(event);
+        }
+        log.info("ChallengeScheduler - checkExpiredTasks() completed");
+    }
+}
