e7d69d51d5c21b3513051acd973dbda5793828e0
WhitePiano <startoflateness@gmail.com>
2025-06-05 17:43:22 +0900

    fix(medium): set region to s3

--- src/main/java/sleppynavigators/studyupbackend/infrastructure/medium/S3Properties.java
+++ src/main/java/sleppynavigators/studyupbackend/infrastructure/medium/S3Properties.java
@@ -1,16 +1,21 @@
 package sleppynavigators.studyupbackend.infrastructure.medium;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "spring.cloud.aws.s3")
-public record S3Properties(@Value("${spring.cloud.aws.region.static}") String region,
-                           String bucket, Long expirationInMinutes) {
+public record S3Properties(String region, String bucket, Long expirationInMinutes) {
 
     public S3Properties {
+        validateRegion(region);
         validateBucket(bucket);
         validateExpirationInMinutes(expirationInMinutes);
+    }
+
+    private void validateRegion(String region) {
+        if (StringUtils.isBlank(region)) {
+            throw new IllegalArgumentException("Region must not be null or empty");
+        }
     }
 
     private void validateBucket(String bucket) {
