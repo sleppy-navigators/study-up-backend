1cc84957c63668a869f70dc5c0a7a68d7679d47c
WhitePiano <startoflateness@gmail.com>
2025-06-10 19:31:29 +0900

    feat(notification): implement message generator for task certify and fail

---
+++ src/main/java/sleppynavigators/studyupbackend/domain/notification/generator/TaskCertifyNotificationMessageGenerator.java
@@ -0,0 +1,24 @@
+package sleppynavigators.studyupbackend.domain.notification.generator;
+
+import org.springframework.stereotype.Component;
+import sleppynavigators.studyupbackend.domain.event.EventType;
+import sleppynavigators.studyupbackend.domain.event.challenge.TaskCertifyEvent;
+import sleppynavigators.studyupbackend.domain.notification.NotificationMessage;
+
+@Component
+public class TaskCertifyNotificationMessageGenerator implements NotificationMessageGenerator<TaskCertifyEvent> {
+
+    private static final String TITLE = "테스크 인증 알림";
+    private static final String MESSAGE_FORMAT = "%s님이 '%s' 테스크를 인증했습니다. (%s)";
+
+    @Override
+    public NotificationMessage generate(TaskCertifyEvent event) {
+        String body = String.format(MESSAGE_FORMAT, event.userName(), event.taskName(), event.challengeName());
+        return new NotificationMessage(TITLE, body, null, null);
+    }
+
+    @Override
+    public EventType supportedEventType() {
+        return EventType.TASK_CERTIFY;
+    }
+}
